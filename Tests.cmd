7884048991abacd58b1f140fa48925eecc3272cc
SkyLined <github@skylined.nl>
2020-06-17 23:10:59 +0200

    Fix tests

--- Tests.cmd
+++ Tests.cmd
@@ -1,49 +1,34 @@
 @ECHO OFF
 SETLOCAL
-SET REDIRECT_STDOUT_FILE_PATH=%TEMP%\zyp Test stdout %RANDOM%.txt
-SET TEST_FILE_PATH=%TEMP%\zyp Test file %RANDOM%.txt
-SET TEST_ZIP_FILE_PATH=%TEMP%\zyp Test file %RANDOM%.zip
+SET TEST_DOWNLOAD_FILE_PATH=%TEMP%\HTTP Test file %RANDOM%.txt
 
 ECHO   * Test version check...
-CALL "%~dp0zyp.cmd" --version
-IF ERRORLEVEL 1 GOTO :ERROR
-CALL "%~dp0unzyp.cmd" --version
+CALL "%~dp0HTTP.cmd" --version
 IF ERRORLEVEL 1 GOTO :ERROR
 
 ECHO   * Test help...
-CALL "%~dp0zyp.cmd" --help
-IF ERRORLEVEL 1 GOTO :ERROR
-CALL "%~dp0unzyp.cmd" --help
+CALL "%~dp0HTTP.cmd" --help
 IF ERRORLEVEL 1 GOTO :ERROR
 
-ECHO   * Zipping test file...
-ECHO Hello, world! >"%TEST_FILE_PATH%"
-IF ERRORLEVEL 1 GOTO :ERROR
-CALL "%~dp0zyp.cmd" "%TEST_FILE_PATH%" "%TEST_ZIP_FILE_PATH%"
-IF NOT %ERRORLEVEL% == 1 GOTO :ERROR
-DEL "%TEST_FILE_PATH%" /Q
-
-ECHO   * Listing zipped test file...
-CALL "%~dp0unzyp.cmd" --list "%TEST_ZIP_FILE_PATH%"
+ECHO   * Test GET http://example.com...
+CALL "%~dp0HTTP.cmd" GET "http://example.com" -db
 IF ERRORLEVEL 1 GOTO :ERROR
 
-ECHO   * Unzipping test file...
-CALL "%~dp0unzyp.cmd" "%TEST_ZIP_FILE_PATH%" "%TEMP%"
-IF NOT %ERRORLEVEL% == 1 GOTO :ERROR
-IF NOT EXIST "%TEST_FILE_PATH%" (
-  ECHO Unzipping did not re-create test file!
-  DEL %TEST_ZIP_FILE_PATH% /Q
-  EXIT /B 1
-)
-DEL "%TEST_FILE_PATH%" /Q
-DEL "%TEST_ZIP_FILE_PATH%" /Q
+ECHO   * Test GET http://example.com...
+CALL "%~dp0HTTP.cmd" GET "http://example.com" --download="%TEST_DOWNLOAD_FILE_PATH%"
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test GET https://example.com...
+CALL "%~dp0HTTP.cmd" GET "https://example.com" --download="%TEST_DOWNLOAD_FILE_PATH%"
+IF ERRORLEVEL 1 GOTO :ERROR
+
+DEL "%TEST_DOWNLOAD_FILE_PATH%" /Q
 
 ECHO + Done.
 EXIT /B 0
 
 :ERROR
   ECHO     - Failed with error level %ERRORLEVEL%
-  IF EXIST "%TEST_FILE_PATH%" DEL "%TEST_FILE_PATH%" /Q
-  IF EXIST "%TEST_ZIP_FILE_PATH%" DEL "%TEST_ZIP_FILE_PATH%" /Q
+  IF EXIST "%TEST_DOWNLOAD_FILE_PATH%" DEL "%TEST_DOWNLOAD_FILE_PATH%" /Q
   ENDLOCAL
   EXIT /B 1
