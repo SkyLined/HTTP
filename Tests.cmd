1572eb71e56ac324374dbef887285be5f16fd64c
SkyLined <github@skylined.nl>
2021-01-29 10:15:51 +0100

    Add dependencies test

--- Tests.cmd
+++ Tests.cmd
@@ -1,5 +1,44 @@
 @ECHO OFF
 SETLOCAL
+IF "%~1" == "--all" (
+  REM If you can add the x86 and x64 binaries of python to the path, or add links to the local folder, tests will be run
+  REM in both
+  WHERE PYTHON_X86 >nul 2>&1
+  IF NOT ERRORLEVEL 0 (
+    ECHO - PYTHON_X86 was not found; not testing both x86 and x64 ISAs.
+  ) ELSE (
+    WHERE PYTHON_X64 >nul 2>&1
+    IF NOT ERRORLEVEL 0 (
+      ECHO - PYTHON_X64 was not found; not testing both x86 and x64 ISAs.
+    ) ELSE (
+      GOTO :TEST_BOTH_ISAS
+    )
+  )
+)
+
+WHERE PYTHON 2>&1 >nul
+IF ERRORLEVEL 1 (
+  ECHO - PYTHON was not found!
+  ENDLOCAL
+  EXIT /B 1
+)
+
+ECHO * Running tests in unknown build of Python...
+CALL PYTHON "%~dpn0\%~n0.py" %*
+IF ERRORLEVEL 1 GOTO :ERROR
+GOTO :ADDITIONAL_TESTS
+
+:TEST_BOTH_ISAS
+  ECHO * Running tests in x86 build of Python...
+  CALL PYTHON_X86 "%~dpn0\%~n0.py" %*
+  IF ERRORLEVEL 1 GOTO :ERROR
+  ECHO * Running tests in x64 build of Python...
+  CALL PYTHON_X64 "%~dpn0\%~n0.py" %*
+  IF ERRORLEVEL 1 GOTO :ERROR
+  ENDLOCAL
+  EXIT /B 0
+
+:ADDITIONAL_TESTS
 SET TEST_DOWNLOAD_FILE_PATH=%TEMP%\HTTP Test file %RANDOM%.txt
 
 ECHO   * Test version check...
