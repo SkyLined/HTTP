4b25f2b9b7763581d6df0bebdf788dc437689519
SkyLined <cojmpjum_github.com@sxd.be>
2023-01-26 09:33:03 +0100

    Adjust code to use new Exception API

--- fOutputExceptionAndExit.py
+++ fOutputExceptionAndExit.py
@@ -10,14 +10,6 @@ def fOutputExceptionAndExit(oException, uExitCode):
       "  ",
       COLOR_INFO, str(oException.sMessage),
     );
-    for (sName, xValue) in oException.dxDetails.items():
-      oConsole.fOutput(
-        "  ",
-        COLOR_INFO, str(sName),
-        COLOR_NORMAL, " = ",
-        COLOR_INFO,
-        COLOR_NORMAL, repr(xValue),
-      );
   else:
     oConsole.fOutput(
       "  ",
