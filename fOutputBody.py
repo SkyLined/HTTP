705a4780967ec003044c0a626fd75299863da1de
SkyLined <cojmpjum_github.com@sxd.be>
2023-04-20 10:19:36 +0200

    Implement HEX dump for http message body

--- fOutputBody.py
+++ fOutputBody.py
@@ -4,16 +4,37 @@ from mCP437 import fsCP437FromByte;
 oConsole = foConsoleLoader();
 
 def fOutputBody(sxBody, bNeedsDecoding, bShowDetails, bOutputEOF, xPrefix = []):
-  if isinstance(sxBody, str):
-    xCR = "\r";
-    xLF = "\n";
-    fsChar = lambda sChar: sChar;
-  else:
+  if isinstance(sxBody, bytes):
+    if any(uByte < 0x20 and uByte not in b"\r\n\t" for uByte in sxBody):
+      # None ASCII bytes: dump as hex.
+      asBytesOutput = [];
+      sCharsOutput = "";
+      uOffset = 0;
+      while uOffset < len(sxBody):
+        # byte
+        asBytesOutput.append("%02X" % sxBody[uOffset]);
+        # char
+        sCharsOutput += fsCP437FromByte(sxBody[uOffset]);
+        uOffset += 1;
+        if len(sCharsOutput) == 16 or uOffset == len(sxBody):
+          oConsole.fOutput(
+            xPrefix,
+            COLOR_BODY, " ".join(asBytesOutput).ljust(2 * 16 + 15),
+            COLOR_NORMAL, "  ",
+            COLOR_BODY_DECODED, sCharsOutput
+          );
+          asBytesOutput = [];
+          sCharsOutput = "";
+      return;
     xCR = ord("\r");
     xLF = ord("\n");
     fsChar = lambda uByte: fsCP437FromByte(uByte);
+  else:
+    xCR = "\r";
+    xLF = "\n";
+    fsChar = lambda sChar: sChar;
   xBodyColor = COLOR_BODY if bNeedsDecoding else COLOR_BODY_DECODED;
-  
+
   sLine = "";
   asEOL = [];
   uIndex = 0;
