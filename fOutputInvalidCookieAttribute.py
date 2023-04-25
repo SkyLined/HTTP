dd3af117fabb64e4830e5dbb386dd06d941c0440
SkyLined <cojmpjum_github.com@sxd.be>
2023-04-25 13:57:17 +0200

    Do not exit when a cookie with a invalid attribute is detected

--- fOutputInvalidCookieAttribute.py
+++ fOutputInvalidCookieAttribute.py
@@ -6,28 +6,27 @@ from mCP437 import fsCP437FromBytesString;
 from mExitCodes import *;
 oConsole = foConsoleLoader();
 
-def fOutputInvalidCookieAttributeAndExit(oCookieStore, oResponse, oURL, oHeader, sbCookieName, sbCookieValue, sbAttributeName, sb0AttributeValue, bIsNameKnown):
+def fOutputInvalidCookieAttribute(oCookieStore, oResponse, oURL, oHeader, sbCookieName, sbCookieValue, sbAttributeName, sb0AttributeValue, bIsNameKnown):
   oConsole.fOutput(
     "      ",
     COLOR_WARNING, CHAR_WARNING,
     COLOR_NORMAL, " Server response from ",
-    COLOR_INFO, fsCP437FromBytesString(oURL.fsbToString()),
-    (
+    COLOR_INFO, fsCP437FromBytesString(oURL.sbAbsolute),
+    [
       COLOR_NORMAL, " contains an ",
       COLOR_INFO, "invalid" if bIsNameKnown else "unknown"
-    ) if sb0AttributeValue is not None else (
+    ] if sb0AttributeValue is not None else (
       COLOR_NORMAL, " is missing a ",
     ),
     COLOR_NORMAL, " cookie attribute ",
     COLOR_INFO if bIsNameKnown else COLOR_WARNING, fsCP437FromBytesString(sbAttributeName),
-    (
+    [
       COLOR_NORMAL, " = ",
       COLOR_WARNING if bIsNameKnown else COLOR_INFO, fsCP437FromBytesString(sb0AttributeValue),
-    ) if sb0AttributeValue is not None else [],
+    ] if sb0AttributeValue is not None else [],
     COLOR_NORMAL, " for cookie ",
     COLOR_INFO, fsCP437FromBytesString(sbCookieName),
     COLOR_NORMAL, " = ",
     COLOR_INFO, fsCP437FromBytesString(sbCookieValue),
     COLOR_NORMAL, ".",
   );
-  sys.exit(guExitCodeNoValidResponseReceived);
