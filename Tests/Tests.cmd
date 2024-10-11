f1ea7f51e82e48acac354f35ed2f5a2282751585
SkyLined <cojmpjum_github.com@sxd.be>
2024-10-11 11:09:09 +0200

    Don't output stdout if everything went well

--- Tests/Tests.cmd
+++ Tests/Tests.cmd
@@ -18,7 +18,6 @@ IF ERRORLEVEL 1 GOTO :ERROR
 ECHO   * Test license update...
 CALL "%~dp0\..\HTTP.cmd" --license-update >"%REDIRECT_STDOUT_FILE_PATH%"
 IF ERRORLEVEL 1 GOTO :ERROR
-
 ECHO   * Test GET http://example.com...
 CALL "%~dp0\..\HTTP.cmd" GET "http://example.com" -db >"%REDIRECT_STDOUT_FILE_PATH%"
 IF ERRORLEVEL 1 GOTO :ERROR
@@ -28,15 +27,13 @@ CALL "%~dp0\..\HTTP.cmd" GET "http://skylined.nl" --debug -r=10 --download="%TES
 IF ERRORLEVEL 1 GOTO :ERROR
 IF NOT EXIST "%TEST_DOWNLOAD_FILE_PATH%" (
   ECHO - Downloaded file "%TEST_DOWNLOAD_FILE_PATH%" not found!
-  IF EXIST "%REDIRECT_STDOUT_FILE_PATH%" (
-    POWERSHELL $OutputEncoding = New-Object -Typename System.Text.UTF8Encoding; Get-Content -Encoding utf8 '"%REDIRECT_STDOUT_FILE_PATH%"'
-  )
 ) ELSE (
   DEL "%TEST_DOWNLOAD_FILE_PATH%" /Q
 )
 IF EXIST "%REDIRECT_STDOUT_FILE_PATH%" (
   DEL "%REDIRECT_STDOUT_FILE_PATH%" /Q
 )
+
 ECHO + Test.cmd completed.
 ENDLOCAL
 EXIT /B 0
--- fInitializeProduct.py
+++ fInitializeProduct.py
@@ -40,6 +40,8 @@
     try:
       oModule = __import__(sModuleName, dict(globals()), {}, [], 0);
     except Exception as oException:
+      if bOptional:
+        return None;
       bModuleNotFound = isinstance(oException, ModuleNotFoundError) and oException.args[0] == "No module named '%s'" % sModuleName;
       if bProductIsAnApplication or bDebugOutput:
         if bModuleNotFound:
@@ -51,8 +53,6 @@
             oException.__class__.__name__,
             str(oException),
           ));
-      if bOptional:
-        return None;
       # Dump exception stack like Python would
       import traceback;
       asExceptionReportLines = traceback.format_exc().split("\n");
