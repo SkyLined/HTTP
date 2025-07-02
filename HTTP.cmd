2c5f77ae947ca8c9911a7f3835801d821ff5e899
SkyLined <jeuuvfww_github.com@sxd.be>
2025-07-02 16:01:35 +0200

    Update HTTP.cmd
    Improve main script

--- HTTP.cmd
+++ HTTP.cmd
@@ -1,36 +1,26 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
 IF DEFINED PYTHON (
-  CALL :CHECK_PYTHON
-  IF NOT ERRORLEVEL 1 GOTO :RUN_PYTHON
+  IF EXIST !PYTHON! GOTO :RUN_PYTHON
 )
 IF DEFINED PYTHONPATH (
-  SET PYTHON=%PYTHONPATH%\python.exe
-  CALL :CHECK_PYTHON
-  IF NOT ERRORLEVEL 1 GOTO :RUN_PYTHON
+  SET PYTHON=!PYTHONPATH!\python.exe
+  IF EXIST !PYTHON! GOTO :RUN_PYTHON
 )
 REM Try to detect the location of python automatically
 FOR /F "usebackq delims=" %%I IN (`where "python" 2^>nul`) DO (
   SET PYTHON="%%~fI"
-  CALL :CHECK_PYTHON
-  IF NOT ERRORLEVEL 1 GOTO :RUN_PYTHON
+  IF EXIST !PYTHON! GOTO :RUN_PYTHON
 )
 REM Check if python is found in its default installation path.
-FOR /D %%I IN ("%LOCALAPPDATA%\Programs\Python\*") DO (
+FOR /D %%I IN ("!LOCALAPPDATA!\Programs\Python\*") DO (
   SET PYTHON="%%~fI\python.exe"
-  CALL :CHECK_PYTHON
-  IF NOT ERRORLEVEL 1 GOTO :RUN_PYTHON
+  IF EXIST !PYTHON! GOTO :RUN_PYTHON
 )
 ECHO - Cannot find python.exe, please set the "PYTHON" environment variable to the
 ECHO   correct path, or add Python to the "PATH" environment variable.
 EXIT /B 1
 
-:CHECK_PYTHON
-  REM Make sure path is quoted and check if it exists.
-  SET PYTHON="%PYTHON:"=%"
-  IF NOT EXIST %PYTHON% EXIT /B 1
-  EXIT /B 0
-
 :RUN_PYTHON
-  %PYTHON% "%~dpn0.py" %*
-  ENDLOCAL & EXIT /B %ERRORLEVEL%
+  CALL !PYTHON! "%~dpn0.py" %*
+  EXIT /B %ERRORLEVEL%
