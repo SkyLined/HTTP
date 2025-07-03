2197571a9bdc46cac706652efb7c083be6e3f711
SkyLined <1959110+SkyLined@users.noreply.github.com>
2025-07-03 16:24:12 +0200

    Update HTTP.cmd

--- HTTP.cmd
+++ HTTP.cmd
@@ -25,7 +25,5 @@ ECHO   correct path, or add Python to the "PATH" environment variable.
 EXIT /B 1
 
 :RUN_PYTHON
-  REM We did not find `/?` in the arguments.
-  ECHO !PYTHON! !sScript! !sArguments!
   CALL !PYTHON! !sScript! !sArguments!
   EXIT /B !ERRORLEVEL!
